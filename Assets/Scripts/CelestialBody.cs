ac0fab04129bfe41d9817bf024d86ec75ba9a350
Bryn <bryndrury@icloud.com>
2024-02-02 12:04:58 +0000

    Verlet Integration Methods

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class CelestialBody : MonoBehaviour
 {
     // Properties of the celestial body
+    public int id;                 // ID of the celestial body
     public string bodyName;        // Name of the celestial body
-    public string mass;             // Mass of the celestial body in kg
-    public string massEarth;        // Mass of the celestial body in Earth masses
-    public string radius;           // Mean radius of the celestial body in km
-    public string radiusEarth;      // Mean radius of the celestial body in Earth radii
-    public string apoapsis;         // Apoapsis of the celestial body's orbit in AU
-    public string periapsis;        // Periapsis of the celestial body's orbit in AU
-    public string argOfPeriapsis;   // Argument of periapsis in degrees
-    public string semiMajorAxis;    // Semi-major axis of the celestial body's orbit in AU
-    public string inclination;      // Inclination of the celestial body's orbit in degrees
-    public string orbitalPeriod;    // Sidereal orbital period in days
-    public string rotationalPeriod; // Sidereal rotational period in days
-    public string axialTilt;        // Axial tilt of the celestial body in degrees
-    public string absoluteMagnitude; // Absolute magnitude
-    public string averageDensity;   // Average density of the celestial body in g/cm^3
-    public string surfaceTemp;      // Surface temperature of the celestial body in K
-    public string surfaceGravity;   // Surface gravity of the celestial body in g
-    public string hasMoons;          // Does the celestial body have moons?
+    public float mass;             // Mass of the celestial body in kg
+    public float massEarth;        // Mass of the celestial body in Earth masses
+    public float radius;           // Mean radius of the celestial body in km
+    public float radiusEarth;      // Mean radius of the celestial body in Earth radii
+    public float apoapsis;         // Apoapsis of the celestial body's orbit in AU
+    public float periapsis;        // Periapsis of the celestial body's orbit in AU
+    public float argOfPeriapsis;   // Argument of periapsis in degrees
+    public float semiMajorAxis;    // Semi-major axis of the celestial body's orbit in AU
+    public float inclination;      // Inclination of the celestial body's orbit in degrees
+    public float orbitalPeriod;    // Sidereal orbital period in days
+    public float rotationalPeriod; // Sidereal rotational period in days
+    public float axialTilt;        // Axial tilt of the celestial body in degrees
+    public float absoluteMagnitude;// Absolute magnitude
+    public float averageDensity;   // Average density of the celestial body in g/cm^3
+    public float surfaceTemp;      // Surface temperature of the celestial body in K
+    public float surfaceGravity;   // Surface gravity of the celestial body in g
+    public int hasMoons;           // Number of moons
+    public Vector3 pos;            // Position vector
+    public Vector3 vel;            // Velocity vector
+    public Vector3 acc;            // Acceleration vector
 
     // Set properties based on data from the CSV file
-    public void SetPropertiesFromData(string mass, string massEarth, string radius, string radiusEarth,
-                                      string apoapsis, string periapsis, string argOfPeriapsis,
-                                      string semiMajorAxis, string inclination, string orbitalPeriod,
-                                      string rotationalPeriod, string axialTilt, string absoluteMagnitude,
-                                      string averageDensity, string surfaceTemp, string surfaceGravity,
-                                      string hasMoons)
+    public void SetPropertiesFromData(int id, 
+                                      float mass, float massEarth, float radius, float radiusEarth,
+                                      float apoapsis, float periapsis, float argOfPeriapsis,
+                                      float semiMajorAxis, float inclination, float orbitalPeriod,
+                                      float rotationalPeriod, float axialTilt, float absoluteMagnitude,
+                                      float averageDensity, float surfaceTemp, float surfaceGravity,
+                                      int hasMoons)
     {
+        this.id = id;
         this.mass = mass;
         this.massEarth = massEarth;
         this.radius = radius;
@@ -51,6 +57,9 @@ public class CelestialBody : MonoBehaviour
         this.hasMoons = hasMoons;
     }
 
-    // Add methods here to simulate the physics
-    
+    public void calculate_initial_position_velocity()
+    {
+        // This website goes the other way, but it might work to reverse the process
+        // https://phys.libretexts.org/Bookshelves/Astronomy__Cosmology/Celestial_Mechanics_(Tatum)/09%3A_The_Two_Body_Problem_in_Two_Dimensions/9.08%3A_Orbital_Elements_and_Velocity_Vector
+    }
 }
--- Assets/Scripts/SolarSystemManager.cs
+++ Assets/Scripts/SolarSystemManager.cs
@@ -15,6 +15,9 @@ public class SolarSystemManager : MonoBehaviour
     [SerializeField]
     public string prefabsFolder;
 
+    // colection of all the celestial bodies
+    public List<CelestialBody> celestialBodiesList = new List<CelestialBody>();
+
     void Start()
     {
         // Populate the dictionary with celestial body names and prefab paths
@@ -48,29 +51,32 @@ public class SolarSystemManager : MonoBehaviour
                 Debug.Log("instantiated");
                 celestialBodyInit.SetActive(true);
                 CelestialBody celestialBodyScript = celestialBodyInit.GetComponent<CelestialBody>();
-
+                
                 // Set properties based on CSV data
                 celestialBodyScript.SetPropertiesFromData(
-                    values[2], // Mass of the celestial body in kg
-                    values[3], // Mass of the celestial body in Earth masses
-                    values[4], // Mean radius of the celestial body in km
-                    values[5], // Mean radius of the celestial body in Earth radii
-                    values[6], // Apoapsis of the celestial body's orbit in AU
-                    values[7], // Periapsis of the celestial body's orbit in AU
-                    values[8], // Argument of periapsis in degrees
-                    values[9], // Semi-major axis of the celestial body's orbit in AU
-                    values[10], // Inclination of the celestial body's orbit in degrees
-                    values[11], // Sidereal orbital period in days
-                    values[12], // Sidereal rotational period in days
-                    values[13], // Axial tilt of the celestial body in degrees
-                    values[14], // Absolute magnitude
-                    values[15], // Average density of the celestial body in g/cm^3
-                    values[16], // Surface temperature of the celestial body in K
-                    values[17], // Surface gravity of the celestial body in g
-                    values[18]  // Moons
+                    i, // ID of the celestial body
+                    float.Parse(values[2]), // Mass of the celestial body in kg
+                    float.Parse(values[3]), // Mass of the celestial body in Earth masses
+                    float.Parse(values[4]), // Mean radius of the celestial body in km
+                    float.Parse(values[5]), // Mean radius of the celestial body in Earth radii
+                    float.Parse(values[6]), // Apoapsis of the celestial body's orbit in AU
+                    float.Parse(values[7]), // Periapsis of the celestial body's orbit in AU
+                    float.Parse(values[8]), // Argument of periapsis in degrees
+                    float.Parse(values[9]), // Semi-major axis of the celestial body's orbit in AU
+                    float.Parse(values[10]), // Inclination of the celestial body's orbit in degrees
+                    float.Parse(values[11]), // Sidereal orbital period in days
+                    float.Parse(values[12]), // Sidereal rotational period in days
+                    float.Parse(values[13]), // Axial tilt of the celestial body in degrees
+                    float.Parse(values[14]), // Absolute magnitude
+                    float.Parse(values[15]), // Average density of the celestial body in g/cm^3
+                    float.Parse(values[16]), // Surface temperature of the celestial body in K
+                    float.Parse(values[17]), // Surface gravity of the celestial body in g
+                    int.Parse(values[18])    // Moons (number of moons)
                 );
 
                 celestialBodyScript.bodyName = celestialBodyName;
+
+                celestialBodiesList.Add(celestialBodyScript);
             }
             else
             {
@@ -95,4 +101,51 @@ public class SolarSystemManager : MonoBehaviour
             celestialBodyPrefabPaths.Add(celestialBodyName, prefabPath);
         }
     }
+
+    public void InitialiseCelestialBodies()
+    {
+        foreach (CelestialBody celestialBody in celestialBodiesList)
+        {
+            celestialBody.calculate_initial_position_velocity();
+        }
+    }
+
+    private void UpdateGravitationalAcceleration(CelestialBody celestialBody)
+    {   
+        celestialBody.acc = Vector3.zero;
+
+        foreach (CelestialBody otherCelestialBody in celestialBodiesList)
+        {
+            if (celestialBody != otherCelestialBody)
+            {
+                Vector3 r = otherCelestialBody.pos - celestialBody.pos;
+                float distance = r.magnitude;
+
+                float force = (6.67430f * Mathf.Pow(10, -11) * otherCelestialBody.mass) / Mathf.Pow(distance, 2);
+                Vector3 accFromOther = r.normalized * force;
+                celestialBody.acc += accFromOther;
+            }
+        }
+    }
+
+    public void VerletUpdateCelestialBodies()
+    {
+        foreach (CelestialBody celestialBody in celestialBodiesList)
+        {
+            // verlet integration
+
+            // half step velocity
+            celestialBody.vel = celestialBody.vel + ( 0.5f * celestialBody.acc * Time.fixedDeltaTime );
+
+            // full step position (using half step velocity)
+            celestialBody.pos = celestialBody.pos + ( celestialBody.vel * Time.fixedDeltaTime );
+
+            // update acceleration (using new position)
+            UpdateGravitationalAcceleration(celestialBody);
+
+            // half step velocity (using updated acceleration)
+            celestialBody.vel = celestialBody.vel + ( 0.5f * celestialBody.acc * Time.fixedDeltaTime );
+        }
+    }
 }
+
