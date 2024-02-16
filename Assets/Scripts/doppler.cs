1c954782344ca4d6133bccf2c2881ad05ecf635f
Iris Adams <117635802+wb21021@users.noreply.github.com>
2024-02-16 14:55:12 +0000

    Edited doppler.cs
    Added velocity and position of both observer and body to be used in doppler.cs

--- Assets/Scripts/doppler.cs
+++ Assets/Scripts/doppler.cs
@@ -1,44 +1,59 @@
 using UnityEngine;
+using UnityEngine.XR;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
-
-// class which assigns the observer variable to the main camera 
-public class AssignObserverToMainCamera : MonoBehaviour
-{
-    public Doppler dopplerScript;
-
-    void Start()
-    {
-        // Find the main camera in the scene
-        GameObject mainCamera = Camera.main.gameObject;
-
-        // Assign the main camera as the observer in the Doppler script
-        dopplerScript.observer = mainCamera;
-    }
-}
 
 
 //defines the class that can be attacjed to GameObjects in the Unity Scene
 public class Doppler : MonoBehaviour
 {
-    public Vector3 velocity;
-    public GameObject observer;
-    public Color baseColor;
+    private GameObject player;
+    public GameObject body;
+
+    private Color baseColor;
     public float maxShift;
 
     private Light bodyLight;
 
-    
+    private Vector3 lastplayerPos;
+    private Vector3 lastbodyPos;
+
     // inititialzes the bodyLight variable with the light component attached to the GameObject and sets the colour to baseColor
     void Start()
     {
-        bodyLight = GetComponent<Light>();
+
+        player = GameObject.Find("Main Camera");
+        
+        bodyLight = body.GetComponent<Light>();
         bodyLight.color = baseColor;
+
+        Debug.Log("COLOR: " + baseColor.ToString());
     }
 
     // called once per frame - calculates the colour shift based on the relative velocity of the body and user
     void Update()
     {
-        Vector3 observerDir = observer.transform.position - transform.position;
+
+        //Find positions and velocities of both the player and the body
+
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerVel = (playerPos - lastplayerPos) / Time.deltaTime;
+
+        lastplayerPos = playerPos;
+        
+        Vector3 bodyPos = body.transform.position;
+        Vector3 bodyVel = (bodyPos - lastbodyPos) / Time.deltaTime;
+
+        lastbodyPos = bodyPos;
+
+        //--------------------------------------------------------
+
+        Vector3 observerDir = player.transform.position - body.transform.position;
+
+        Vector3 velocity = new Vector3(10,10,10); //TEMPORARY, PLEASE REMOVE
+
         float relativeVelocity = Vector3.Dot(velocity, observerDir.normalized);
 
         // Calculate color shift
@@ -57,6 +72,7 @@ public class Doppler : MonoBehaviour
             shiftedColor.b -= shiftAmount;
         }
 
+        Debug.Log("SHIFTED COLOUR: " + shiftedColor.ToString());
         bodyLight.color = shiftedColor;
     }
 }
